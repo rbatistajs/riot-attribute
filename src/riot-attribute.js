e0aeda8e630e7b783a81af79d219649f5884026f
Robson Batista <robsonbackend@gmail.com>
2016-12-25 00:45:00 -0200

    Change compile attributes

--- src/riot-attribute.js
+++ src/riot-attribute.js
@@ -3,6 +3,31 @@ const REGEX_ATTR = '=?(?:"([^"]*)"|\'([^\']*)\'|({[^}]*}))?'
 
 let __ATTR_IMPL = {}, __CACHE_ATTR = []
 
+const each = function() {
+    Array.prototype.forEach.call(...arguments)
+}
+
+
+const setProperty = function(o, s, v) {
+    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    s = s.replace(/^\./, '');           // strip a leading dot
+    let a = s.split('.');
+    for (let i = 0, n = a.length; i < n; ++i) {
+        let k = a[i];
+        if (k in o) {
+            if(i == n-1){
+                Object.defineProperty(o, k, {
+                  'value': v
+                })
+            }
+            o = o[k];
+        } else {
+            return;
+        }
+    }
+    return o;
+}
+
 const getImplTagNames = () => {
     let tagNames = util.tags.selectTags().split(',')
     if(!tagNames[0])
@@ -20,59 +45,119 @@ const getImplTags = (tagNames) => {
     })
 }
 
-const compileAttr = (attr, impl) => {
 
-    let nextId = -1
-    let implAttrs = []
-    let regAttr = new RegExp(attr.name+REGEX_ATTR, 'g')
+const Attr = function(attr, el, expr, parent){
+    let self = this
 
-    impl.tmpl = impl.tmpl.replace(regAttr, function(){
-        nextId++
-        let ref = '__'+attr.name+'__id__'+nextId
+    let $inst = Object.create(attr.opts)
 
-        implAttrs.push({
-            name: attr.name,
-            constructor: attr.constructor,
-            ref: ref,
-            expr: arguments[1] || arguments[2] || arguments[3] || ""
+    parent = parent || self.parent
+
+    each(Object.keys(parent), (key) => {
+        if(self.hasOwnProperty(key) || Object.getPrototypeOf(self).hasOwnProperty(key))
+            return
+
+        Object.defineProperty(Object.getPrototypeOf(self), key, {
+            get: function(){
+                return parent[key]
+            },
+            set: function(value){
+                parent[key] = value
+            }
         })
-        return ref
     })
 
+    expr = expr || el.getAttribute(attr.name)
 
-    let cachefn = impl.fn
+    self._attr = $inst
 
-    impl.fn = function(opts){
-
-        if(!implAttrs.length){
-            return cachefn.apply(this, arguments)
+    let _attr = {
+        name: attr.name,
+        get value(){
+            return util.tmpl(expr, self)
+        },
+        set value(v){
+            let m = new RegExp("{([\\w\\. \\[\\]']*)}").exec(expr)
+            if(m && m[1]){
+                setProperty(self, m[1], v)
+            }
         }
-
-        this.on('mount', function () {
-            console.log(implAttrs)
-            implAttrs.forEach((attr) =>{
-                let el = document.querySelector('['+attr.ref+']')
-                el.removeAttribute(attr.ref)
-                new Attr(attr, el, this)
-            })
-        })
-
-        return cachefn.apply(this, arguments)
     }
-    
-    return impl;
+
+    $inst.init.apply(self, [this.root, _attr])
+
 }
 
 
-const compileAttrs = (impl) => {
+let nextId = -1
+const parseAttrInTag = (attr, impl) => {
+        let selector= '['+attr.name+']'
+        let implAttrs = []
+        let regAttr = new RegExp(attr.name+REGEX_ATTR, 'g')
+
+        let $parse = document.createElement('div')
+        $parse.innerHTML = impl.tmpl;
+        let elems = $parse.querySelectorAll(selector)
+
+        each(elems, (el) => {
+            nextId++
+            let ref = '__'+attr.name+'__id__'+nextId
+            let expr = el.getAttribute(attr.name)
+
+            if(attr.opts.compile)
+                attr.opts.compile.apply(attr.opts, [el])
+
+            el.removeAttribute(attr.name)
+
+            let tmpl = ''
+
+            if(attr.opts.tmpl)
+                tmpl = attr.opts.tmpl
+            else
+                tmpl = el.innerHTML
+                               .replace(new RegExp('each'+REGEX_ATTR, 'g'), '')
+                               .replace(new RegExp('if'+REGEX_ATTR, 'g'), '')
+
+            el.setAttribute('data-is', ref)
+            let cachefn = impl.fn;
+            cacheTag(ref, tmpl, '', '', function(){
+                Attr.apply(this, [attr, this.root, expr, this.parent])
+            })
+        })
+
+        impl.tmpl = $parse.innerHTML
+
+        return impl
+}
+
+
+const parseAttrsInTag = (impl) => {
     let nextId = -1
     let implAttrs = [];
 
     Object.keys(__ATTR_IMPL).forEach((key) => {
-        impl = compileAttr(__ATTR_IMPL[key], impl)
+        impl = parseAttrInTag(__ATTR_IMPL[key], impl)
     })
 
     return impl
+}
+
+const cacheTag2 = riot.tag2;
+riot.tag2 = function(name, tmpl, css, attrs, fn){
+    fn = fn || function(){}
+    let impl = parseAttrsInTag({name, tmpl, css, attrs, fn})
+    cacheTag2.apply(riot,
+        Object.keys(impl).map(key => impl[key]))
+    return name;
+}
+
+const cacheTag = riot.tag;
+riot.tag = function(name, tmpl, css, attrs, fn){
+    fn = fn || function(){}
+    let impl = parseAttrsInTag({name, tmpl, css, attrs, fn})
+    cacheTag.apply(riot,
+        Object.keys(impl).map(key => impl[key]))
+    return name;
 }
 
 const selectAttrs = (attrs, tag) => {
@@ -92,39 +177,16 @@ const selectAttrs = (attrs, tag) => {
     })
 }
 
-class Attr {
-    constructor(attr, el, tag){
-        riot.observable(this)
-        this.value = util.tmpl(attr.expr, tag || {})
-        this.element = el
+const parseAttr = (attr, el, target) => {
 
-        if(tag)
-            this.tag = tag
+    nextId++
+    let ref = '__'+attr.name+'__id__'+nextId
 
-        this._attr = attr
-        this.root = el
+    cacheTag(ref, attr.opts.tmpl || util.dom.getOuterHTML(el), '', '', function(){
+        Attr.apply(this, [attr, el, el.getAttribute(attr.name), target])
+    })
 
-        if(util.check.isFunction(this.value))
-            el.setAttribute(attr.name, '')
-        else
-            el.setAttribute(attr.name, this.value)
-
-        attr.constructor.apply(this,[el,this.value])
-
-        __CACHE_ATTR.push(this)
-
-        if(!tag)
-            return
-
-        tag.on('update', () => {
-            this.trigger('update', this.value)
-        })
-
-        tag.on('unmount', () => {
-            this.trigger('unmount', this.value)
-        })
-    }
-
+    return ref
 }
 
 riot.mountAttr = function(selector){
@@ -135,53 +197,69 @@ riot.mountAttr = function(selector){
 
     let elements = document.querySelectorAll(selector)
 
-    Array.prototype.forEach.call(elements, (el) => {
-
-        if(__CACHE_ATTR.filter(a=> a.root === el).length)
-            return
-
+    each(elements, (el) => {
         Object.keys(__ATTR_IMPL).forEach((name) => {
             if(!el.hasAttribute(name))
                 return
+            let attr = __ATTR_IMPL[name];
 
-            let attr = __ATTR_IMPL[name]
+            if(attr.opts.compile)
+                attr.opts.compile.apply(attr.opts, [el, el._tag])
 
-            new Attr({
-                name: name,
-                constructor: attr.constructor,
-                expr: el.getAttribute(name)
-            }, el, el._tag)
+            if(el._tag){
+                if(attr.opts.tmpl){
+                    let dom = document.createElement('span')
+                    let ref = parseAttr(attr, el, {})
+
+                    dom.appendChild(el.cloneNode(true))
+
+                    if(attr.opts.target){
+                        try {
+                            document.querySelector(attr.opts.target).appendChild(dom)
+                        } catch (e){}
+                    }else{
+                        el.appendChild(dom)
+                    }
+
+                    util.tags.mountTo(dom, ref, {}, {})
+                }else{
+
+                    if(attr.opts.target){
+                        try {
+                            document.querySelector(attr.opts.target).appendChild(el)
+                        } catch (e){}
+                    }
+
+                    Attr.apply(el._tag, [__ATTR_IMPL[name], el, null, el._tag])
+                    el._tag.update()
+                }
+            }else{
+                let ref = parseAttr(attr, el, {})
+
+                if(attr.opts.target){
+                    try {
+                        document.querySelector(attr.opts.target).appendChild(el)
+                    } catch (e){}
+                }
+
+                util.tags.mountTo(el, ref, {}, {})
+
+            }
         })
-
     })
 }
 
-const cacheTag2 = riot.tag2;
-riot.tag2 = function(name, tmpl, css, attrs, fn){
-    fn = fn || function(){}
-    let impl = compileAttrs({name, tmpl, css, attrs, fn})
-    cacheTag2.apply(riot,
-        Object.keys(impl).map(key => impl[key]))
-    return name;
-}
-
-const cacheTag = riot.tag;
-riot.tag = function(name, tmpl, css, attrs, fn){
-    fn = fn || function(){}
-    let impl = compileAttrs({name, tmpl, css, attrs, fn})
-    cacheTag.apply(riot,
-        Object.keys(impl).map(key => impl[key]))
-    return name;
-}
-
-riot.attr = (name, constructor) => {
-    __ATTR_IMPL[name] = {name, constructor}
+riot.attr = (name, opts) => {
+    __ATTR_IMPL[name] = {name, opts}
     let tagNames = getImplTagNames()
+
     if(!tagNames)
         return
 
     let impls = getImplTags(tagNames)
-    impls.forEach((impl) => compileAttr(__ATTR_IMPL[name], impl))
+    impls.forEach((impl) => {
+        parseAttrInTag(__ATTR_IMPL[name], impl)
+    })
 }
 
 export default riot.attr
